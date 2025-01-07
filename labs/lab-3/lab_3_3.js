b23e5c7ea855fda51b18b8271cdfaa479227e486
phuongthao2012 <phuongthao2012@gmail.com>
2025-01-07 17:31:23 +0100

    lab3 ver2

--- labs/lab-3/lab_3_3.js
+++ labs/lab-3/lab_3_3.js
@@ -7,8 +7,8 @@
  */
 let intArr = [12, 34, 1, 16, 28];
 let length = intArr.length;
-for (let index = 0; index < length -1; index++) {
-    for (let index2 = 0; index2 < length -1 - index; index2++) {
+for (let index = 0; index < length; index++) {
+    for (let index2 = 0; index2 < length - index; index2++) {
         if (intArr[index2] > intArr[index2 + 1]) {
             let temp = intArr[index2];
             intArr[index2] = intArr[index2 + 1];
--- utils/ArrayHandling.js
+++ utils/ArrayHandling.js
@@ -6,8 +6,8 @@
 // let intArr = [12, 34, 1, 16, 28];
 function sortArray(arr) {
     let length = arr.length;
-    for (let index = 0; index < length -1; index++) {
-        for (let index2 = 0; index2 < length -1 - index; index2++) {
+    for (let index = 0; index < length; index++) {
+        for (let index2 = 0; index2 < length - index; index2++) {
             if (arr[index2] > arr[index2 + 1]) {
                 let temp = arr[index2];
                 arr[index2] = arr[index2 + 1];
