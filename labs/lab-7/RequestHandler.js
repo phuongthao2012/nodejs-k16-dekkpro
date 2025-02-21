0e2bbdac7331a6ec25d8a079c7cf4852f6071fca
phuongthao2012 <phuongthao2012@gmail.com>
2025-02-21 05:02:10 +0100

    fix bug lab 7 - OOP

--- labs/lab-7/RequestHandler.js
+++ labs/lab-7/RequestHandler.js
@@ -10,19 +10,26 @@ const Post = require('./Post'); // Import class Post
 
 class RequestHandler {
     // fetch all Post with userID and set into Post object
+    // constructor this object for handle the POST which is raw data
     async _getAllPosts(POST_ENDPOINT) {
         try {
             const response = await fetch(POST_ENDPOINT);
             const allPosts = await response.json();
+            // this time normaly have value: array of raw object
+            //    console.log(allPosts);
 
             return allPosts
+                // create array of POST object
                 .map(post => new Post(post.userId, post.id, post.title, post.body));
         } catch (error) {
             console.error('Error fetching data:', error);
             return [];
         }
     }
+    // this method for handle the POST after construction
+
     async printTargetPost(POST_ENDPOINT, userId, postId) {
+        // handle the POST, add raw data into Post object
         const allPosts = await this._getAllPosts(POST_ENDPOINT);
         const userPosts = allPosts.filter(post => post.userId === userId);
         const targetPost = userPosts.find(post => post.postId === postId);
@@ -35,6 +42,7 @@ class RequestHandler {
         }
     }
 
+    // this method for handle the POST after construction
     async printAllPosts(POST_ENDPOINT, userId) {
         const allPosts = await this._getAllPosts(POST_ENDPOINT);
         const userPosts = allPosts.filter(post => post.userId === userId);
